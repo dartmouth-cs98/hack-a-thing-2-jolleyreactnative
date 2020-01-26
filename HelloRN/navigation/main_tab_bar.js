fe72d2d0d04cdd604ec5aa6983aaba23ca7b0d71
Liam Jolley <liamoj10@gmail.com>
2020-01-26 13:10:55 -0500

    Adds weird runtime error on emulator.

---
+++ HelloRN/navigation/main_tab_bar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createAppContainer} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { View, Text } from 'react-native';
+
+const AboutTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
+};
+
+const SearchTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
+};
+
+
+const MainTabBar = createBottomTabNavigator({
+  SearchTab,
+  AboutTab,
+}, {
+  initialRouteName: 'SearchTab',
+});
+
+
+export default createAppContainer(MainTabBar);
